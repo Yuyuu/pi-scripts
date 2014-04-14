fb9ced3f9ee2a762715cccc202299d0be1aeb08a
Vincent Tertre <vincent.tertre@ticetime.com>
2014-04-14 23:31:59 +0200

    Create dl dir if it does not exist

--- xdcc.sh
+++ xdcc.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+ROOT_DIR=/home/pi/dev/xdcc
+LISTS_DIR=$ROOT_DIR/lists
 
-java -jar ~/dev/xdcc/xdcc-pi.jar 8089
+if [ ! -e $LISTS_DIR ] || [ ! -d $LISTS_DIR ] ; then
+	echo "Creating dir : $LISTS_DIR"
+	mkdir -p $LISTS_DIR
+fi
+
+if [ -e $ROOT_DIR/xdcc-pi.jar ] ; then
+	echo "Executing jar file"
+	java -jar $ROOT_DIR/xdcc-pi.jar 8089 &
+fi
