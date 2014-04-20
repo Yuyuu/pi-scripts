69d6363e3f1685c9b46c8b5e32a17252d7a9ae70
Yuyuu <fkinrandomstuff@gmail.com>
2014-04-20 19:21:18 +0000

    add log dir

--- xdcc.sh
+++ xdcc.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 ROOT_DIR=/home/pi/prod/xdcc
 LISTS_DIR=$ROOT_DIR/lists
+LOGS_DIR=$ROOT_DIR/logs
 
 if [ ! -e $LISTS_DIR ] || [ ! -d $LISTS_DIR ] ; then
 	echo "Creating dir : $LISTS_DIR"
 	mkdir -p $LISTS_DIR
 fi
 
+if [ ! -e $LOGS_DIR ] || [ ! -d $LOGS_DIR ] ; then
+        echo "Creating dir : $LOG_DIR"
+        mkdir -p $LOGS_DIR
+fi
+
 if [ -f $ROOT_DIR/xdcc-pi.log ] ; then
-	mv $ROOT_DIR/xdcc-pi.log $ROOT_DIR/`date "+%d_%m_%y_%H_%M_%S"`.log
+	echo "Archiving $ROOT_DIR/xdcc-pi.log to $LOGS_DIR"
+	mv $ROOT_DIR/xdcc-pi.log $LOGS_DIR/`date "+%d_%m_%y_%H_%M_%S"`.log
 fi
 
 if [ -e $ROOT_DIR/xdcc-pi.jar ] ; then
