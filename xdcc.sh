95b4710f8791a031bddd60610a9c774a490c791f
Yuyuu <fkinrandomstuff@gmail.com>
2014-05-17 17:36:15 +0000

    remove lists dir creation

--- xdcc.sh
+++ xdcc.sh
@@ -2,27 +2,23 @@
 ROOT_DIR=/home/pi/prod/xdcc
 LISTS_DIR=$ROOT_DIR/lists
 LOGS_DIR=$ROOT_DIR/logs
-VERSION_DIR=$ROOT_DIR/xdcc-api-xdcc-api-$1
+VERSION_DIR=$ROOT_DIR/xdcc-api-$1
 
 if [ -z $1 ] ; then
 	echo "No version specified"
 	exit
 fi
 
-if [ ! -e $LISTS_DIR ] || [ ! -d $LISTS_DIR ] ; then
-	echo "Creating dir : $LISTS_DIR"
-	mkdir -p $LISTS_DIR
-fi
-
 if [ ! -e $VERSION_DIR ] || [ ! -d $VERSION_DIR ] ; then
-	echo "Downloading api version $1"
+	echo "Downloading API version $1"
 	wget --directory-prefix=$ROOT_DIR https://github.com/Yuyuu/xdcc-api/archive/xdcc-api-$1.zip
 	unzip -d $ROOT_DIR xdcc-api-$1.zip
+	mv $ROOT_DIR/xdcc-api-xdcc-api-$1 $VERSION_DIR
 	rm -f $ROOT_DIR/xdcc-api-$1.zip
 fi
 
 if [ ! -e $LOGS_DIR ] || [ ! -d $LOGS_DIR ] ; then
-        echo "Creating dir : $LOG_DIR"
+        echo "Creating dir: $LOG_DIR"
         mkdir -p $LOGS_DIR
 fi
 
@@ -45,3 +41,4 @@ fi
 
 
 # TODO Force delete and redownload version
+
